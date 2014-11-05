5bf8e2a07e166d63ae30d46914b548e550dd65b6
skbohrer <skbohrer@gmail.com>
2014-11-04 22:34:31 -0500

    count position calls, log to scroller

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Short test of geolocation plugin.
 Basic page with a button to call navigator.geolocation.getCurrentPosition 
 and show the results in an alert.
 
-Sample code copied directly from plugin docs file 
+Sample code from plugin docs file 
 cordova-plugin-geolocation/doc/ ndex.md at
 https://github.com/apache/cordova-plugin-geolocation
--- gpstest.js
+++ gpstest.js
@@ -1,32 +1,39 @@
 'use strict';
 
+var theCnt = 0;
 
 // onSuccess Callback
 // This method accepts a Position object, which contains the
 // current GPS coordinates
 //
-var onSuccess = function(position) {
-    alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
+var onSuccess = function(position, idx) {
+    var outStr = 
+		  'Request #' + idx + '<br>' +
+		  'Latitude: '          + position.coords.latitude          + '<br>' +
+          'Longitude: '         + position.coords.longitude         + '<br>' +
+          'Altitude: '          + position.coords.altitude          + '<br>' +
+          'Accuracy: '          + position.coords.accuracy          + '<br>' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '<br>' +
+          'Heading: '           + position.coords.heading           + '<br>' +
+          'Speed: '             + position.coords.speed             + '<br>' +
+          'Timestamp: '         + position.timestamp                + '<br><br>';
+	
+	document.getElementById('wout').innerHTML += outStr;
 };
 
 // onError Callback receives a PositionError object
 //
 function onError(error) {
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
+    var outStr = 'Error code: '    + error.code    + '<br>' +
+          'message: ' + error.message + '<br><br>';
+	document.getElementById('wout').innerHTML += outStr;
 }
 
 
 function doBtnClick () {
-   navigator.geolocation.getCurrentPosition(onSuccess, onError, 
+   navigator.geolocation.getCurrentPosition(function(position) {onSuccess(position, +theCnt);}, onError, 
 								{ maximumAge: 3000, timeout: 5000, enableHighAccuracy: true });
+	theCnt += 1;
 }
 
 // Call on Android device ready event
