091d7091c048749534d549558e28e4099e858daa
skbohrer <skbohrer@gmail.com>
2014-11-05 19:57:39 -0500

    add time stamps

--- gpstest.js
+++ gpstest.js
@@ -2,13 +2,31 @@
 
 var theCnt = 0;
 
+
+function getTS() {
+	var d = new Date();
+	
+	function twoDgt(n) {
+		var retval; 	
+		if ( n < 10 ) {
+			retval = "0" + n.toString();
+		}
+		else {
+			retval = n.toString();
+		}
+		return retval;
+	}
+	
+	return twoDgt(d.getHours()) + ":" + twoDgt(d.getMinutes()) + ":" + twoDgt(d.getSeconds());
+}
+
 // onSuccess Callback
 // This method accepts a Position object, which contains the
 // current GPS coordinates
 //
-var onSuccess = function(position, idx) {
+function onSuccess(position, idx, ts) {
     var outStr = 
-		  'Request #' + idx + '<br>' +
+		  'Request #' + idx + ' @ ' + ts 							+ '<br>' +
 		  'Latitude: '          + position.coords.latitude          + '<br>' +
           'Longitude: '         + position.coords.longitude         + '<br>' +
           'Altitude: '          + position.coords.altitude          + '<br>' +
@@ -16,10 +34,11 @@ var onSuccess = function(position, idx) {
           'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '<br>' +
           'Heading: '           + position.coords.heading           + '<br>' +
           'Speed: '             + position.coords.speed             + '<br>' +
-          'Timestamp: '         + position.timestamp                + '<br><br>';
+          'Timestamp: '         + position.timestamp                + '<br>' +
+		  'done @ ' + getTS() + '<br><br>';
 	
 	document.getElementById('wout').innerHTML += outStr;
-};
+}
 
 // onError Callback receives a PositionError object
 //
@@ -31,8 +50,8 @@ function onError(error) {
 
 
 function doBtnClick () {
-   navigator.geolocation.getCurrentPosition(function(position) {onSuccess(position, +theCnt);}, onError, 
-								{ maximumAge: 3000, timeout: 5000, enableHighAccuracy: true });
+   navigator.geolocation.getCurrentPosition(function(position) {onSuccess(position, +theCnt, getTS());}, onError, 
+								{ maximumAge: 1000, timeout: 5000, enableHighAccuracy: true });
 	theCnt += 1;
 }
 
