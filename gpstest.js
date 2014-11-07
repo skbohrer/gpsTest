19336a0011380b71ed4c8c8fadc8eeba518a344d
skbohrer <skbohrer@gmail.com>
2014-11-06 19:08:56 -0500

    changed to block variable
    functions are now elements of gps object, which is how they would need
    to be structured for main RideCheck code.

--- gpstest.js
+++ gpstest.js
@@ -1,61 +1,63 @@
 'use strict';
 
-var theCnt = 0;
+var gps = {
+	theCnt: 0,
 
 
-function getTS() {
-	var d = new Date();
-	
-	function twoDgt(n) {
-		var retval; 	
-		if ( n < 10 ) {
-			retval = "0" + n.toString();
+	getTS: function () {
+		var d = new Date();
+		
+		function twoDgt(n) {
+			var retval; 	
+			if ( n < 10 ) {
+				retval = "0" + n.toString();
+			}
+			else {
+				retval = n.toString();
+			}
+			return retval;
 		}
-		else {
-			retval = n.toString();
-		}
-		return retval;
-	}
-	
-	return twoDgt(d.getHours()) + ":" + twoDgt(d.getMinutes()) + ":" + twoDgt(d.getSeconds());
-}
+		
+		return twoDgt(d.getHours()) + ":" + twoDgt(d.getMinutes()) + ":" + twoDgt(d.getSeconds());
+	},
 
 // onSuccess Callback
 // This method accepts a Position object, which contains the
 // current GPS coordinates
 //
-var doOnSuccess = function (idx, ts) {
+	doOnSuccess: function (idx, ts) {
 
-	var onSuccess = function (position) {
-		var outStr = 
-			'Request #' + idx + ' @ ' + ts 							+ '<br>' +
-			'Lat/Long: ' + position.coords.latitude + ' ' + position.coords.longitude + '<br>' +
-			'Accuracy: ' + position.coords.accuracy          + '<br>' +
-			'done @ ' + getTS() + '<br><br>';
-	
-		document.getElementById('wout').innerHTML += outStr;
-	};
-	return onSuccess;
-};
+		var onSuccess = function (position) {
+			var outStr = 
+				'Request #' + idx + ' @ ' + ts 	+ '<br>' +
+				'Lat/Long: ' + position.coords.latitude + ' ' + position.coords.longitude + '<br>' +
+				'Accuracy: ' + position.coords.accuracy + '<br>' +
+				'done @ ' + gps.getTS() + '<br><br>';
+		
+			document.getElementById('wout').innerHTML += outStr;
+		};
+		return onSuccess;
+	},
 
 // onError Callback receives a PositionError object
 //
-function onError(error) {
-    var outStr = 'Error code: '    + error.code    + '<br>' +
-          'message: ' + error.message + '<br><br>';
-	document.getElementById('wout').innerHTML += outStr;
-}
+	onError: function(error) {
+		var outStr = 'Error code: '    + error.code    + '<br>' +
+			  'message: ' + error.message + '<br><br>';
+		document.getElementById('wout').innerHTML += outStr;
+	},
 
 
-function doBtnClick () {
-   navigator.geolocation.getCurrentPosition(doOnSuccess(+theCnt, getTS()), onError, 
-								{ maximumAge: 1000, timeout: 5000, enableHighAccuracy: true });
-	theCnt += 1;
-}
+	doBtnClick: function () {
+	   navigator.geolocation.getCurrentPosition(gps.doOnSuccess(+gps.theCnt, gps.getTS()), gps.onError, 
+									{ maximumAge: 1000, timeout: 5000, enableHighAccuracy: true });
+		gps.theCnt += 1;
+	}
+};
 
 // Call on Android device ready event
 function init() {
-  document.getElementById('getLoc').onclick = doBtnClick;
+  document.getElementById('getLoc').onclick = gps.doBtnClick;
 }
 
 // Wait for device API libraries to load
