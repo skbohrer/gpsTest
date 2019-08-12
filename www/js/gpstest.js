23c4c9b3b7e0a560290527c34d998761f8626b57
Steve Bohrer <skbohrer@gmail.com>
2019-08-11 23:56:45 -0400

    calc close stops

--- www/js/gpstest.js
+++ www/js/gpstest.js
@@ -2,7 +2,34 @@
 
 var gps = {
 	theCnt: 0,
-                                                                              
+
+	stops: [	
+		{ stop: "Back Bay", lat: 42.347441, lon: -71.074592 },
+		{ stop: "Huntington Ave @ Belvidere St", lat: 42.345344, lon: -71.082045 },
+		{ stop: "Huntington Ave @ Gainsborough St", lat: 42.341443, lon: -71.086788 },
+		{ stop: "Huntington Ave @ Opera Pl", lat: 42.340553, lon: -71.088908 },
+		{ stop: "Huntington Ave @ Forsyth Way", lat: 42.339219, lon: -71.092168 },
+		{ stop: "Huntington Ave @ Louis Prang St", lat: 42.337836, lon: -71.095698 },
+		{ stop: "Huntington Ave @ Evans Way", lat: 42.336885, lon: -71.098086 },
+		{ stop: "Huntington Ave @ Longwood Ave", lat: 42.336046, lon: -71.100251 },
+		{ stop: "677 Huntington Ave", lat: 42.335111, lon: -71.102717 },
+		{ stop: "Huntington Ave @ Fenwood Rd", lat: 42.333739, lon: -71.105717 },
+		{ stop: "835 Huntington Ave opp Parker Hill Ave", lat: 42.33328, lon: -71.109273 },
+		{ stop: "S Huntington Ave @ Huntington Ave", lat: 42.331803, lon: -71.112061 },
+		{ stop: "105 S Huntington Ave", lat: 42.329368, lon: -71.111046 },
+		{ stop: "S Huntington Ave opp VA Hospital", lat: 42.326625, lon: -71.111437 },
+		{ stop: "S Huntington Ave @ Bynner St", lat: 42.323986, lon: -71.112327 },
+		{ stop: "S Huntington Ave @ Perkins St", lat: 42.32172, lon: -71.112195 },
+		{ stop: "S Huntington Ave @ Moraine St", lat: 42.319618, lon: -71.111729 },
+		{ stop: "Centre St @ Lochstead Ave", lat: 42.317365, lon: -71.113229 },
+		{ stop: "Centre St @ Myrtle St", lat: 42.313306, lon: -71.11416 },
+		{ stop: "Centre St @ Burroughs St", lat: 42.311466, lon: -71.1145 },
+		{ stop: "775 Centre St", lat: 42.309949, lon: -71.115345 },
+		{ stop: "South St @ Bardwell St", lat: 42.308378, lon: -71.115619 },
+		{ stop: "South St @ Jamaica St", lat: 42.306131, lon: -71.115091 },
+		{ stop: "South St @ St Rose St", lat: 42.304396, lon: -71.114769 },
+		{ stop: "Forest Hills", lat: 42.300749, lon: -71.114314 }
+	], 
 
 	getTS: function () {
 		var d = new Date();
@@ -131,8 +158,26 @@ var gps = {
  	setPoint: function() {
 	   navigator.geolocation.getCurrentPosition(gps.doOnSuccess(-1, gps.getTS()), gps.onError, 
 									{ maximumAge: 2000, timeout: 10000, enableHighAccuracy: true });
-	}
+	},
 
+	getStopDist: function(obj, lat, lon) {
+		return gps.dist(obj.lat, obj.lon, lat, lon);
+	},
+
+	minStopDist: function (curLat, curLon) {
+		var i, curDist, minDist, minStop = null;
+
+		for (i = 0; i < gps.stops.length; i += 1) {
+			curDist = gps.getStopDist(gps.stops[i], curLat, curLon);
+			if (! minStop || curDist < minDist) {
+				minStop = gps.stops[i];
+				minDist = curDist;
+			}
+		}
+		if ( minStop ) {
+			gps.msg('Closest stop: ' + minStop.stop + ' Dist:' + minDist);
+		}
+	}
 };
 
 // Call on Android device ready event
