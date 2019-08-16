c7ff9431c51cbfbcc4f26d512d1fbd1f45c7c291
Steve Bohrer <skbohrer@gmail.com>
2019-08-15 22:25:09 -0400

    increase timeout for gps only

--- www/js/gpstest.js
+++ www/js/gpstest.js
@@ -14,56 +14,56 @@ var gps = {
 	theCnt: 0,
 
 	stops: [	
-		{ stop: "Back Bay", lat: 42.347441, lon: -71.074592 },
-		{ stop: "Huntington Ave @ Belvidere St", lat: 42.345344, lon: -71.082045 },
-		{ stop: "Huntington Ave @ Gainsborough St", lat: 42.341443, lon: -71.086788 },
-		{ stop: "Huntington Ave @ Opera Pl", lat: 42.340553, lon: -71.088908 },
-		{ stop: "Huntington Ave @ Forsyth Way", lat: 42.339219, lon: -71.092168 },
-		{ stop: "Huntington Ave @ Louis Prang St", lat: 42.337836, lon: -71.095698 },
-		{ stop: "Huntington Ave @ Evans Way", lat: 42.336885, lon: -71.098086 },
-		{ stop: "Huntington Ave @ Longwood Ave", lat: 42.336046, lon: -71.100251 },
-		{ stop: "677 Huntington Ave", lat: 42.335111, lon: -71.102717 },
-		{ stop: "Huntington Ave @ Fenwood Rd", lat: 42.333739, lon: -71.105717 },
-		{ stop: "835 Huntington Ave opp Parker Hill Ave", lat: 42.33328, lon: -71.109273 },
-		{ stop: "S Huntington Ave @ Huntington Ave", lat: 42.331803, lon: -71.112061 },
-		{ stop: "105 S Huntington Ave", lat: 42.329368, lon: -71.111046 },
-		{ stop: "S Huntington Ave opp VA Hospital", lat: 42.326625, lon: -71.111437 },
-		{ stop: "S Huntington Ave @ Bynner St", lat: 42.323986, lon: -71.112327 },
-		{ stop: "S Huntington Ave @ Perkins St", lat: 42.32172, lon: -71.112195 },
-		{ stop: "S Huntington Ave @ Moraine St", lat: 42.319618, lon: -71.111729 },
-		{ stop: "Centre St @ Lochstead Ave", lat: 42.317365, lon: -71.113229 },
-		{ stop: "Centre St @ Myrtle St", lat: 42.313306, lon: -71.11416 },
-		{ stop: "Centre St @ Burroughs St", lat: 42.311466, lon: -71.1145 },
-		{ stop: "775 Centre St", lat: 42.309949, lon: -71.115345 },
-		{ stop: "South St @ Bardwell St", lat: 42.308378, lon: -71.115619 },
-		{ stop: "South St @ Jamaica St", lat: 42.306131, lon: -71.115091 },
-		{ stop: "South St @ St Rose St", lat: 42.304396, lon: -71.114769 },
-		{ stop: "Forest Hills", lat: 42.300749, lon: -71.114314 },
+		{ stop: "Back Bay", lat: 42.347441, lon: -71.074592 , state: st.noState },
+		{ stop: "Huntington Ave @ Belvidere St", lat: 42.345344, lon: -71.082045 , state: st.noState },
+		{ stop: "Huntington Ave @ Gainsborough St", lat: 42.341443, lon: -71.086788 , state: st.noState },
+		{ stop: "Huntington Ave @ Opera Pl", lat: 42.340553, lon: -71.088908 , state: st.noState },
+		{ stop: "Huntington Ave @ Forsyth Way", lat: 42.339219, lon: -71.092168 , state: st.noState },
+		{ stop: "Huntington Ave @ Louis Prang St", lat: 42.337836, lon: -71.095698 , state: st.noState },
+		{ stop: "Huntington Ave @ Evans Way", lat: 42.336885, lon: -71.098086 , state: st.noState },
+		{ stop: "Huntington Ave @ Longwood Ave", lat: 42.336046, lon: -71.100251 , state: st.noState },
+		{ stop: "677 Huntington Ave", lat: 42.335111, lon: -71.102717 , state: st.noState },
+		{ stop: "Huntington Ave @ Fenwood Rd", lat: 42.333739, lon: -71.105717 , state: st.noState },
+		{ stop: "835 Huntington Ave opp Parker Hill Ave", lat: 42.33328, lon: -71.109273 , state: st.noState },
+		{ stop: "S Huntington Ave @ Huntington Ave", lat: 42.331803, lon: -71.112061 , state: st.noState },
+		{ stop: "105 S Huntington Ave", lat: 42.329368, lon: -71.111046 , state: st.noState },
+		{ stop: "S Huntington Ave opp VA Hospital", lat: 42.326625, lon: -71.111437 , state: st.noState },
+		{ stop: "S Huntington Ave @ Bynner St", lat: 42.323986, lon: -71.112327 , state: st.noState },
+		{ stop: "S Huntington Ave @ Perkins St", lat: 42.32172, lon: -71.112195 , state: st.noState },
+		{ stop: "S Huntington Ave @ Moraine St", lat: 42.319618, lon: -71.111729 , state: st.noState },
+		{ stop: "Centre St @ Lochstead Ave", lat: 42.317365, lon: -71.113229 , state: st.noState },
+		{ stop: "Centre St @ Myrtle St", lat: 42.313306, lon: -71.11416 , state: st.noState },
+		{ stop: "Centre St @ Burroughs St", lat: 42.311466, lon: -71.1145 , state: st.noState },
+		{ stop: "775 Centre St", lat: 42.309949, lon: -71.115345 , state: st.noState },
+		{ stop: "South St @ Bardwell St", lat: 42.308378, lon: -71.115619 , state: st.noState },
+		{ stop: "South St @ Jamaica St", lat: 42.306131, lon: -71.115091 , state: st.noState },
+		{ stop: "South St @ St Rose St", lat: 42.304396, lon: -71.114769 , state: st.noState },
+		{ stop: "Forest Hills", lat: 42.300749, lon: -71.114314 , state: st.noState },
 
 
-		{ stop: "Inb. Forest Hills", lat:42.300749, lon:-71.114314 },
-		{ stop: "Inb. South St @ Spalding St", lat:42.303741, lon:-71.114635 },
-		{ stop: "Inb. South St @ Child St", lat:42.306545, lon:-71.115144 },
-		{ stop: "Inb. South St @ Sedgwick St", lat:42.308588, lon:-71.115487 },
-		{ stop: "Inb. Centre St @ Seaverns Ave", lat:42.312198, lon:-71.114144 },
-		{ stop: "Inb. Centre St @ Saint John St", lat:42.314197, lon:-71.114036 },
-		{ stop: "Inb. Centre St @ Robinwood Ave", lat:42.316951, lon:-71.113368 },
-		{ stop: "Inb. Centre St @ Roseway St", lat:42.318993, lon:-71.111932 },
-		{ stop: "Inb. S Huntington Ave @ Perkins St", lat:42.321698, lon:-71.112025 },
-		{ stop: "Inb. S Huntington Ave @ Bynner St", lat:42.32381, lon:-71.112191 },
-		{ stop: "Inb. S Huntington Ave @ VA Hospital", lat:42.326594, lon:-71.111284 },
-		{ stop: "Inb. S Huntington Ave @ Heath St", lat:42.328697, lon:-71.110671 },
-		{ stop: "Inb. 100 S Huntington Ave", lat:42.329401, lon:-71.11093 },
-		{ stop: "Inb. S Huntington Ave @ Huntington Ave", lat:42.33187, lon:-71.111965 },
-		{ stop: "Inb. Huntington Ave @ Parker Hill Ave", lat:42.333092, lon:-71.109678 },
-		{ stop: "Inb. Huntington Ave opp Fenwood Rd", lat:42.333704, lon:-71.105586 },
-		{ stop: "Inb. Huntington Ave @ Wigglesworth St", lat:42.334833, lon:-71.102701 },
-		{ stop: "Inb. Huntington Ave @ Longwood Ave", lat:42.335831, lon:-71.099868 },
-		{ stop: "Inb. Huntington Ave @ Ruggles St", lat:42.337612, lon:-71.095267 },
-		{ stop: "Inb. 360 Huntington Ave", lat:42.34016, lon:-71.088899 },
-		{ stop: "Inb. Huntington Ave @ Gainsborough St", lat:42.341244, lon:-71.08651 },
-		{ stop: "Inb. Belvidere St @ Huntington Ave", lat:42.345533, lon:-71.081932 },
-		{ stop: "Inb. Back Bay", lat:42.347441, lon:-71.074592 }
+		{ stop: "Inb. Forest Hills", lat:42.300749, lon:-71.114314 , state: st.noState },
+		{ stop: "Inb. South St @ Spalding St", lat:42.303741, lon:-71.114635 , state: st.noState },
+		{ stop: "Inb. South St @ Child St", lat:42.306545, lon:-71.115144 , state: st.noState },
+		{ stop: "Inb. South St @ Sedgwick St", lat:42.308588, lon:-71.115487 , state: st.noState },
+		{ stop: "Inb. Centre St @ Seaverns Ave", lat:42.312198, lon:-71.114144 , state: st.noState },
+		{ stop: "Inb. Centre St @ Saint John St", lat:42.314197, lon:-71.114036 , state: st.noState },
+		{ stop: "Inb. Centre St @ Robinwood Ave", lat:42.316951, lon:-71.113368 , state: st.noState },
+		{ stop: "Inb. Centre St @ Roseway St", lat:42.318993, lon:-71.111932 , state: st.noState },
+		{ stop: "Inb. S Huntington Ave @ Perkins St", lat:42.321698, lon:-71.112025 , state: st.noState },
+		{ stop: "Inb. S Huntington Ave @ Bynner St", lat:42.32381, lon:-71.112191 , state: st.noState },
+		{ stop: "Inb. S Huntington Ave @ VA Hospital", lat:42.326594, lon:-71.111284 , state: st.noState },
+		{ stop: "Inb. S Huntington Ave @ Heath St", lat:42.328697, lon:-71.110671 , state: st.noState },
+		{ stop: "Inb. 100 S Huntington Ave", lat:42.329401, lon:-71.11093 , state: st.noState },
+		{ stop: "Inb. S Huntington Ave @ Huntington Ave", lat:42.33187, lon:-71.111965 , state: st.noState },
+		{ stop: "Inb. Huntington Ave @ Parker Hill Ave", lat:42.333092, lon:-71.109678 , state: st.noState },
+		{ stop: "Inb. Huntington Ave opp Fenwood Rd", lat:42.333704, lon:-71.105586 , state: st.noState },
+		{ stop: "Inb. Huntington Ave @ Wigglesworth St", lat:42.334833, lon:-71.102701 , state: st.noState },
+		{ stop: "Inb. Huntington Ave @ Longwood Ave", lat:42.335831, lon:-71.099868 , state: st.noState },
+		{ stop: "Inb. Huntington Ave @ Ruggles St", lat:42.337612, lon:-71.095267 , state: st.noState },
+		{ stop: "Inb. 360 Huntington Ave", lat:42.34016, lon:-71.088899 , state: st.noState },
+		{ stop: "Inb. Huntington Ave @ Gainsborough St", lat:42.341244, lon:-71.08651 , state: st.noState },
+		{ stop: "Inb. Belvidere St @ Huntington Ave", lat:42.345533, lon:-71.081932 , state: st.noState },
+		{ stop: "Inb. Back Bay", lat:42.347441, lon:-71.074592, state: st.noState }
 
 	], 
 
@@ -176,7 +176,7 @@ var gps = {
 
 	doBtnClick: function() {
 	   navigator.geolocation.getCurrentPosition(gps.doOnSuccess(+gps.theCnt, gps.getTS()), gps.onError, 
-									{ maximumAge: 5000, timeout: 10000, enableHighAccuracy: true });
+									{ maximumAge: 5000, timeout: 120000, enableHighAccuracy: true });
 		gps.theCnt += 1; 
 	},
 
@@ -189,14 +189,14 @@ var gps = {
 			gps.watchID = null;
 		} else {
 			gps.watchID = navigator.geolocation.watchPosition(gps.repeatSuccess, gps.onError, 
-									{ maximumAge: 2000, timeout: 30000, enableHighAccuracy: true });
+									{ maximumAge: 5000, timeout: 120000, enableHighAccuracy: true });
 			gps.msg('StartWatch, ID is: '+ gps.watchID);
 		}
 	},
 
  	setPoint: function() {
 	   navigator.geolocation.getCurrentPosition(gps.doOnSuccess(-1, gps.getTS()), gps.onError, 
-									{ maximumAge: 5000, timeout: 10000, enableHighAccuracy: true });
+									{ maximumAge: 5000, timeout: 120000, enableHighAccuracy: true });
 	},
 
 	getStopDist: function(obj, lat, lon) {
